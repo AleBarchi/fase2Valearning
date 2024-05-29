a0a00562b9a40134c753ed29642505f00a716577
Barchi <121292066+AleBarchi@users.noreply.github.com>
2024-05-29 09:30:10 +0200

    Add files via upload
    ho provato a fare la visualizzazione degli es dopo che tu mi mandi il json

--- index.js
+++ index.js
@@ -1,83 +1,161 @@
-function creaEsVF(esercizio) {
-	let aus = ` <div class="card esVF">
-                    <div class="card-header">
-                        <span>Esercizio n° ${esercizio.id}    </span>`;
-	for (let i of esercizio.argomento) aus += `<val-tag1></val-tag1>`;
+window.onload = function() {
+    fetch('index.php')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data received:', data);
+            //uso il vettore json
+            
+            creaCompComuni(data);
+           
 
-	aus += `    <span class="material-symbols-outlined expandMore">
-                    expand_more
-                </span>
-            </div>
-                <div class="card-body">
-                    <div class="card-text">
-                        <p>${esercizio.consegna}</p> 
-                        <div>
-                            <input type="radio" id="v" name="${esercizio.id}">
-                            <label for="v">V</label>
-                            <input type="radio" id="f" name="${esercizio.id}">
-                            <label for="f">F</label>
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+
+};
+
+function creaCompComuni(esercizio) {
+
+    let aus = `<div id="contenitore">
+                <span>${esercizio.titolo}</span><br>
+                <span>livello esercizio: ${esercizio.livello}</span>`;
+
+    aus += creaEs(esercizio);
+
+    aus +=`<button class="btn btn-primary" onclick="correggi(${esercizio})">Correggi</button>
+    </div>`;
+
+    document.getElementsByTagName("main").innerHTML = aus;
+
+}
+
+function creaEs(esercizio) {
+
+
+    switch(esercizio.tipo){
+
+        case "veroFalso":
+            return creaEsVF(esercizio.domande);
+
+        case "sceltaMultipla":
+            return creaEsRM(esercizio.domande);
+
+        case "testoBucato":
+            return creaEsTB(esercizio.domande);
+            
+    }
+
+}
+
+
+function creaEsVF(domande) {
+
+    let aus;
+
+    for (let i = 0; i < domande.length; i++) {
+
+        aus += ` <div class="card esVF">
+                        <div class="card-header">
+                            <span>Esercizio n° ${domande[i].DomID}    </span>`;
+
+        aus += `    <span class="material-symbols-outlined expandMore">
+                        expand_more
+                    </span>
+                </div>
+                    <div class="card-body">
+                        <div class="card-text">
+                            <p>${domande[i].testo}</p> 
+                            <div>
+                                <input type="radio" id="v" name="${domande[i].testo}">
+                                <label for="v">V</label>
+                                <input type="radio" id="f" name="${domande[i].testo}">
+                                <label for="f">F</label>
+                            </div>
                         </div>
                     </div>
-                    <button class="btn btn-primary" onclick="correggi(${esercizio})">Correggi</button>
-                </div>
-            </div>`;
+                </div>`;
+    }
 
-	document.getElementsByClassName("esercizioVF")[0].innerHTML = aus;
+    return aus;
 }
 
-function creaEsTB(esercizio) {
-	let aus = ` <div class="card esTB">
-                    <div class="card-header">
-                        <span>Esercizio n° ${esercizio.id}    </span>`;
-	for (let i of esercizio.argomento) {
-		aus += `<val-tag1></val-tag1>`;
-	}
-	aus += `<span class="material-symbols-outlined expandMore">
-                expand_more
-            </span>
-            </div>
+function creaEsTB(domande) {
+
+    let aus;
+
+
+    for (let i = 0; i < domande.length; i++) {
+
+        let testo =  domande[i].testo.split(";");
+
+        aus += ` <div class="card esTB">
+                        <div class="card-header">
+                            <span>Esercizio n° ${domande[i].DomID}    </span>`;
+
+        aus += `<span class="material-symbols-outlined expandMore">
+                    expand_more
+                </span>
+                </div>
+                    <div class="card-body">
+                    <p>inserisci la parola mancante nel testo</p> 
+                    <div class="card-text">
+                        <p>${testo[0]}
+                        <input type="text">
+                        ${testo[1]}</p>
+                    </div>
+                </div>
+            </div>`;
+    
+    }
+
+    return aus;
+
+}
+
+function creaEsRM(domande) {
+    let aus;
+
+    for (let i = 0; i < domande.length; i++) {
+
+        let risp = domande[i].elencoRisposte.split(";");
+
+        let rispostaCorretta = risp[0];
+
+        //jack fai lo shuffle!!!!
+        //shuffle(risp);
+
+        aus += ` <div class="card esRM">
+                        <div class="card-header">
+                            <span>Esercizio n° ${domande[i].DomID}     </span>`;
+
+        aus += `<span class="material-symbols-outlined expandMore">
+                    expand_more
+                </span>
+                </div>
                 <div class="card-body">
-                <p>${esercizio.consegna}</p> 
-                <div class="card-text">
-                    <p>${esercizio.testoPrima}
-                    <input type="text">
-                    ${esercizio.testoDopo}</p>
+                    <div class="card-text">
+                        <p>${domande[i].testo}</p> 
+                        <div>`;
+
+        for (let i in risp){
+            aus += `
+            <input type="radio" id="ris${i}" name="${domande[i].DomID}">
+            <label for="ris${i}">${risp[i]}</label><br>`;
+        }
+        aus += `        </div>
+                    </div>
                 </div>
-                <button class="btn btn-primary" onclick="correggi(${esercizio})">Correggi</button>
-            </div>
-        </div>`;
+                </div>`;
+    }
 
-	document.getElementsByClassName("esercizioTB")[0].innerHTML = aus;
-}
+    return aus;
 
-function creaEsRM(esercizio) {
-	let aus = ` <div class="card esRM">
-                    <div class="card-header">
-                        <span>Esercizio n° ${esercizio.id}    </span>`;
-	for (let i of esercizio.argomento) {
-		aus += `<val-tag1></val-tag1>`;
-	}
-	aus += `<span class="material-symbols-outlined expandMore">
-                expand_more
-            </span>
-            </div>
-            <div class="card-body">
-                <div class="card-text">
-                    <p>${esercizio.consegna}</p> 
-                    <div>`;
-
-	for (let i in esercizio.scelte) {
-		aus += `
-        <input type="radio" id="ris${i}" name="${esercizio.id}">
-        <label for="ris${i}">${esercizio.scelte[i]}</label><br>`;
-	}
-	aus += `        </div>
-                </div>
-                <button class="btn btn-primary" onclick="correggi(${esercizio})">Correggi</button>
-            </div>
-            </div>`;
-
-	document.getElementsByClassName("esercizioRM")[0].innerHTML = aus;
 }
 
 function showModal() {
